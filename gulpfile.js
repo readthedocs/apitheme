35c1ca749e37ed32c12e3603a89378d50b7e3a43
Anthony Johnson <aj@ohess.org>
2016-10-24 16:43:14 -0700

    Automate rebuilding demo docs

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,14 @@
 var gulp = require('gulp'),
     browserify = require('browserify'),
     debowerify = require('debowerify'),
-    source = require('vinyl-source-stream'),
-    buffer = require('vinyl-buffer'),
+    jshint = require('gulp-jshint'),
     less = require('gulp-less'),
     bower_resolve = require('less-plugin-bower-resolve'),
+    source = require('vinyl-source-stream'),
+    buffer = require('vinyl-buffer'),
     util = require('gulp-util'),
-    jshint = require('gulp-jshint'),
+    watch = require('gulp-watch'),
+    run = require('gulp-run'),
     es = require('event-stream');
 
 /* Tasks */
@@ -21,6 +23,28 @@ gulp.task('build', function () {
         build_js(),
         build_css()
     ]);
+});
+
+gulp.task('dev', function (done) {
+    util.log('Continually building source files');
+
+    demo_doc_stream = build_demo_docs();
+
+    watch(['static/css/*'], {awaitWriteFinish: true}, function (file) {
+        util.log('File changed:', file.path);
+        build_css()
+            .pipe(demo_doc_stream);
+    });
+    watch(['static/js/*'], {awaitWriteFinish: true}, function (file) {
+        util.log('File changed:', file.path);
+        build_js()
+            .pipe(demo_doc_stream);
+    });
+    watch(['apitheme/*.html'], {awaitWriteFinish: true}, function (file) {
+        util.log('File changed:', file.path);
+        es.readArray([file])
+            .pipe(demo_doc_stream);
+    });
 });
 
 /* Task functions */
@@ -58,4 +82,14 @@ function build_css () {
             util.log('LESS error:', ev.message);
         })
         .pipe(gulp.dest('apitheme/static/css/'));
+}
+
+function build_demo_docs (stream, done) {
+    return es.wait(function (err, body) {
+        util.log('Building demo docs');
+        run('make html', {cwd: 'demo/', verbosity: 1})
+            .exec('', function (err) {
+                if (done) { done(err); }
+            });
+    });
 }
