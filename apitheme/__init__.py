a1a0f693d2ad2a87c2836f89f262566e4bb7d3ac
Eric Holscher <eric@ericholscher.com>
2016-11-04 12:32:42 -0700

    Fix theme pathing

--- apitheme/__init__.py
+++ apitheme/__init__.py
@@ -3,5 +3,6 @@ import os
 
 def get_html_theme_path():
     """Return list of HTML theme paths."""
-    cur_dir = os.path.abspath(os.path.dirname(__file__))
-    return [cur_dir]
+    cur_dir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+    return cur_dir
+
